ccf58201d089c6f0a05a901406de70d5c09e4885
shaheen-kubihal <shaheenkubihal@gmail.com>
2025-04-24 15:42:23 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Arithmetic Operations:");
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+    }
+}
+
