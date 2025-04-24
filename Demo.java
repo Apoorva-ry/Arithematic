0ba0d6404f6e8b86240f31ef8c05abc7770ae3b0
Apoorva-ry <apoorvayaligar33@gmail.com>
2025-04-24 16:48:08 +0530

    Demo.java

--- Demo.java
+++ Demo.java
@@ -6,8 +6,8 @@ public class Demo {
         System.out.println("Arithmetic Operations:");
         System.out.println("Addition: " + (a + b));
         System.out.println("Subtraction: " + (a - b));
-        System.out.println("Subtraction: " + (a * b));
-        System.out.println("Subtraction: " + (a / b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
 
     }
 }
